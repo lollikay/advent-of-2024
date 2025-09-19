1f36b96a5f14b32facfc4beef99571cfbe7d3c84
Lotta <lottakayumova@gmail.com>
2025-09-19 10:53:09 +0200

    completed css challenge 05

---
+++ src/features/css-5/ui/index.tsx
@@ -0,0 +1,76 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Html from '@kitajs/html';
+import '@shared/ui/container/style.scss';
+import './style.scss';
+
+export const CssChallenge05 = () => {
+  return (
+    <>
+      <section class="container css-challenge-05">
+        <h1>CSS Challenge 05: footer appears on scroll</h1>
+        <p>
+          The challenge relies on @container queries and the scroll-state feature.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{' '}
+          Iusto, maiores dolore. Natus tenetur, voluptas soluta quis minima labore veniam{' '}
+          quasi consequuntur, placeat praesentium quae impedit{' '}
+          at molestiae necessitatibus neque id?
+        </p>
+        <p>
+          Ducimus quibusdam totam et nesciunt iure, autem repellendus corrupti molestiae?{' '}
+          Ipsum tenetur fugit deserunt soluta laboriosam modi sunt, natus porro quidem aut.{' '}
+          Nobis, possimus. Impedit suscipit dicta laudantium consequatur qui.
+        </p>
+        <p>
+          Nulla, pariatur laboriosam? Fuga a quos esse odit ipsum earum ipsa{' '}
+          cupiditate quia est voluptatem quae nostrum ut aliquid id,{' '}
+          facere possimus eveniet magni, blanditiis saepe odio obcaecati alias aspernatur.
+        </p>
+        <p>
+          Sed distinctio temporibus sequi mollitia tenetur sunt quibusdam at.{' '}
+          Aperiam repudiandae molestias quia? Aperiam minima, itaque natus esse velit{' '}
+          numquam quam impedit assumenda est laboriosam reprehenderit{' '}
+          et temporibus nulla voluptatibus.
+        </p>
+        <p>
+          Harum quibusdam iusto non suscipit voluptatum assumenda unde odio laborum excepturi,{' '}
+          vitae explicabo, optio ex dolor error vero illo impedit? Eaque vero voluptate saepe beatae.{' '}
+          Cupiditate ullam debitis eum sed.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{' '}
+          Iusto, maiores dolore. Natus tenetur, voluptas soluta quis minima labore veniam{' '}
+          quasi consequuntur, placeat praesentium quae impedit{' '}
+          at molestiae necessitatibus neque id?
+        </p>
+        <p>
+          Ducimus quibusdam totam et nesciunt iure, autem repellendus corrupti molestiae?{' '}
+          Ipsum tenetur fugit deserunt soluta laboriosam modi sunt, natus porro quidem aut.{' '}
+          Nobis, possimus. Impedit suscipit dicta laudantium consequatur qui.
+        </p>
+        <p>
+          Nulla, pariatur laboriosam? Fuga a quos esse odit ipsum earum ipsa{' '}
+          cupiditate quia est voluptatem quae nostrum ut aliquid id,{' '}
+          facere possimus eveniet magni, blanditiis saepe odio obcaecati alias aspernatur.
+        </p>
+        <p>
+          Sed distinctio temporibus sequi mollitia tenetur sunt quibusdam at.{' '}
+          Aperiam repudiandae molestias quia? Aperiam minima, itaque natus esse velit{' '}
+          numquam quam impedit assumenda est laboriosam reprehenderit{' '}
+          et temporibus nulla voluptatibus.
+        </p>
+        <p>
+          Harum quibusdam iusto non suscipit voluptatum assumenda unde odio laborum excepturi,{' '}
+          vitae explicabo, optio ex dolor error vero illo impedit? Eaque vero voluptate saepe beatae.{' '}
+          Cupiditate ullam debitis eum sed.
+        </p>
+        <footer>
+          <p>
+            &copy; Advent of 2024 - Css Challenge 05: footer appears on scroll
+          </p>
+        </footer>
+      </section>
+    </>
+  )
+}
