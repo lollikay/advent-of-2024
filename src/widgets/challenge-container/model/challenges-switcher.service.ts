f443eeb429a70103a6a7ef7bc6d9bc0b21b3ae7f
lollikay <47807069+lollikay@users.noreply.github.com>
2025-09-19 09:05:33 +0200

    completed css challenge 04 (#7)

--- src/widgets/challenge-container/model/challenges-switcher.service.ts
+++ src/widgets/challenge-container/model/challenges-switcher.service.ts
@@ -1,6 +1,7 @@
 import { CssChallenge01 } from "@features/css-1/ui";
 import { CssChallenge02 } from "@features/css-2/ui";
 import { CssChallenge03 } from "@features/css-3/ui";
+import { CssChallenge04 } from "@features/css-4/ui";
 import { NoChallenge } from "@features/no-challenge/ui";
 
 export const challengeContainerId = 'challenge-container';
@@ -12,6 +13,7 @@ export class ChallengesSwitcherService {
     1: CssChallenge01,
     2: CssChallenge02,
     3: CssChallenge03,
+    4: CssChallenge04,
   };
 
   constructor() { }
