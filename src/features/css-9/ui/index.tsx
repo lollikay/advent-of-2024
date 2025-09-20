aff20a68f7acc2ec6fa5248256c791949bc3dba5
Lotta <lottakayumova@gmail.com>
2025-09-20 19:41:28 +0200

    added dark section

--- src/features/css-9/ui/index.tsx
+++ src/features/css-9/ui/index.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Html from '@kitajs/html';
 import '@shared/ui/container/style.scss';
+import '@shared/ui/margins/style.scss';
 import style from './style.module.scss';
 
 export const CssChallenge09 = () => {
   return (
     <>
-      <section class="container">
+      <section class="container mb-5">
         <h1>CSS Challenge 09: button with gradient border</h1>
         <button
           class={style.button}
@@ -14,6 +15,16 @@ export const CssChallenge09 = () => {
           CSS rocks!
         </button>
       </section>
+      <div style="background: #000; color: #fff; padding: 1.5rem 0;">
+        <section class="container">
+          <h1>Button is transparent</h1>
+          <button
+            class={style.button}
+          >
+            CSS rocks!
+          </button>
+        </section>
+      </div>
     </>
   )
 }
