19eac565b2aee20a10c796c740a512bf2c273aaa
Lotta <lottakayumova@gmail.com>
2025-10-02 07:55:09 +0200

    completed css challenge 16

---
+++ src/features/css-16/ui/index.tsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Html from '@kitajs/html';
+import '@shared/ui/container/style.scss';
+import '@shared/ui/margins/style.scss';
+import style from './style.module.scss';
+import { TextContent } from '@features/text-content/ui';
+
+export const CssChallenge16 = () => {
+  return (
+    <>
+      <div class="container">
+        <h2>CSS Challenge 16: Custom scrollbar</h2>
+      </div>
+      <div class={style.pageContents}>
+        <section class="container">
+          <TextContent />
+        </section>
+      </div>
+    </>
+  )
+};
